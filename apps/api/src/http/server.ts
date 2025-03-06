ef48f9bc98d11adfb9ac7cadf85bfdefe17a6efe
codesofC <inelusjudelin01@gmail.com>
2025-03-05 22:21:12 -0300

    jinelus : feat(routes): register new routes for projects, members, invites, and organization billing

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,12 +18,28 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { getOrganizationBilling } from './routes/billing/get-organization-billing'
+import { acceptInvite } from './routes/invites/accept-invite'
+import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { getPendingInvites } from './routes/invites/get-pending-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
+import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/organizations/create-organization'
 import { getMembership } from './routes/organizations/get-membership'
 import { getOrganization } from './routes/organizations/get-organization'
 import { getOrganizations } from './routes/organizations/get-organizations'
 import { shutdownOrganization } from './routes/organizations/shutdown-organization'
+import { transferOrganization } from './routes/organizations/transfer-organization'
 import { updateOrganization } from './routes/organizations/update-organization'
+import { createProject } from './routes/projects/create-project'
+import { fetchProjects } from './routes/projects/fetch-projects'
+import { getProject } from './routes/projects/get-project'
+import { updateProject } from './routes/projects/update-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -80,6 +96,22 @@ app.register(getOrganizations)
 app.register(getMembership)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
+app.register(transferOrganization)
+app.register(createProject)
+app.register(getProject)
+app.register(fetchProjects)
+app.register(updateProject)
+app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
+app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
+app.register(getPendingInvites)
+app.register(getOrganizationBilling)
 
 app
   .listen({
